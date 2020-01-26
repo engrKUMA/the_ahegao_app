55de054dcb80755f9108d946c43f92b537312f70
Alfred <alfred.salumbides@gmail.com>
2020-01-26 10:08:26 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('The Ahegao App'),
+          backgroundColor: Colors.black12,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('asset/images/alex oppai.jpg'),
+          ),
+        ),
+        bottomNavigationBar: Text('this boobies is all you want v2'),
+      ),
+    ),
+  );
+}
